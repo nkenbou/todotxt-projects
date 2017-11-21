0a010c83564a3ea438f5910c143d181523f26e1e
nkenbou <nkenbou@gmail.com>
2017-11-21 20:47:04 +0900

    Write library file header

--- todotxt-projects.el
+++ todotxt-projects.el
@@ -1,9 +1,36 @@
-;;; Extentions for todotxt.el
-;;
-;; Functions:
-;; - Switch to other todo.txt which exists now.
+;;; todotxt-projects.el --- Add project management features to todotxt.el -*- lexical-binding: t -*-
+
+;; Copyright (C) 2017 Kenichi Sone
+
+;; Author: Kenichi Sone <nkenbou@gmail.com>
+;; Version: 0.1.0
+;; Package-Requires: ((todotxt "0.2.4"))
+;; Keywords: todo.txt, todotxt, todotxt.el
+;; URL: https://github.com/nkenbou/todotxt-projects
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This package provides following extentions for todotxt.el.
+;; - Switch to another project's todo.txt which exists now.
+;; - Move tasks to another project's todo.txt.
 ;; - Transpose task (Up/Down)
 
+;;; Code:
+
+(require 'cl-lib)
 (require 'todotxt)
 
 (defvar todotxt-location "~/todo")
